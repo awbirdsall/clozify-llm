2df230287a579337922bb63cf7a9ab5d069d639b
awbirdsall <abirdsall@gmail.com>
2023-03-25 10:22:59 -0400

    Add data prep tools

---
+++ src/clozify_llm/extract/extract_cloze.py
@@ -0,0 +1,25 @@
+"""extract-cloze.py Utility for extracting tabular cloze data from raw json
+
+Assumes raw json matches list of console log objects from clozemaster
+"""
+import pandas as pd
+
+
+def extract_cloze(raw_input: list[dict]) -> pd.DataFrame:
+    """Extract contents from list of clozemaster collection pages
+
+    Each dict is a page containing a list of collectionClozeSentences.
+
+    Each output row has the contents of one sentence, including "text",
+    "translation", "cloze" (detected from {{ }} via regex), and "collection"
+    (extracted from "collection.name").
+    """
+    df = pd.json_normalize(
+        raw_input,
+        record_path="collectionClozeSentences",
+        meta=["collection"],
+    )
+    df["collection"] = df["collection"].apply(lambda x: x["name"])
+    cloze_finder = r"\{\{(?P<cloze>\w*)\}\}"
+    df["cloze"] = df["text"].str.extract(cloze_finder)
+    return df
