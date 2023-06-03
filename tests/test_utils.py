25de734c86dbcadd063e8f311b1406ea727d2fce
awbirdsall <abirdsall@gmail.com>
2023-06-03 08:26:32 -0400

    Set up CI with Github actions
    - Add Github actions workflow
    - set pytest testpaths
    - Add missing tests/

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+"""test_utils.py Unit testing of utils
+"""
+from clozify_llm.utils import format_completion
+
+
+def test_format_completion():
+    csv_str = format_completion("text", 'translation,"quotation",commas', "cloze")
+    expected = '"text","translation,\\"quotation\\",commas","cloze"'
+    assert csv_str == expected
