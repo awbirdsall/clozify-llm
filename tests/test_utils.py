d8cae00d504a5e4a7aab22fedc874fd119b4bca2
awbirdsall <abirdsall@gmail.com>
2023-06-03 08:56:25 -0400

    Fix broken test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,10 @@
 """test_utils.py Unit testing of utils
 """
+from clozify_llm.constants import END_STR
 from clozify_llm.utils import format_completion
 
 
 def test_format_completion():
     csv_str = format_completion("text", 'translation,"quotation",commas', "cloze")
-    expected = '"text","translation,\\"quotation\\",commas","cloze"'
+    expected = ' "text","translation,\\"quotation\\",commas","cloze"' + END_STR
     assert csv_str == expected
