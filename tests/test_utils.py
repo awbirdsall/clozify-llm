31e4b45a1692f3673239ac1e05c06510a0dcb773
awbirdsall <abirdsall@gmail.com>
2023-06-11 09:43:13 -0400

    Add tests for embed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,37 +2,8 @@
 """
 from unittest.mock import patch
 
-import pytest
-
 from clozify_llm.constants import END_STR, PROMPT_SEPARATOR
 from clozify_llm.utils import format_completion, format_prompt, get_emb, get_embs
-
-
-@pytest.fixture
-def embedding_vals():
-    """Mock values for embedding"""
-    return [
-        0.0023064255,
-        -0.009327292,
-        -0.0028842222,
-    ]
-
-
-@pytest.fixture
-def embedding_response(embedding_vals):
-    """Mock response from Embedding API"""
-    return {
-        "object": "list",
-        "data": [
-            {
-                "object": "embedding",
-                "embedding": embedding_vals,
-                "index": 0,
-            }
-        ],
-        "model": "text-embedding-ada-002",
-        "usage": {"prompt_tokens": 8, "total_tokens": 8},
-    }
 
 
 def test_format_completion():
